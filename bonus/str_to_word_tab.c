8814c12c207da6a61b56513e4a05ed08ac3d906d
Matteo Gassend <matteo.gassend@epitech.eu>
2018-03-06 17:28:25 +0100

    added test for str to word tab

--- bonus/str_to_word_tab.c
+++ bonus/str_to_word_tab.c
@@ -13,9 +13,14 @@ Test(str_to_word_tab, simpleTest)
 	char **expected;
 	char **got;
 
-	expected = my_str_to_word_tab("This is a simple test");
+	expected = malloc(sizeof(char*) * 6);
+	expected[0] = "This";
+	expected[1] = "is";
+	expected[2] = "a";
+	expected[3] = "simple";
+	expected[4] = "test";
 	got = my_str_to_word_tab("This is a simple test");
 	for (int i = 0; got[i] != NULL; i++) {
 		cr_assert_str_eq(expected[i], got[i]);
 	}
-}
+}	
